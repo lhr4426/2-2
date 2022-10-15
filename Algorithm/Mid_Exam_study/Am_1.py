14f020f9be273bfaf566a74e4b7c0ace1546ce80
lhr4426 <20211924@edu.hanbat.ac.kr>
2022-10-15 23:27:42 +0900

    test

--- Algorithm/Mid_Exam_study/Am_1.py
+++ Algorithm/Mid_Exam_study/Am_1.py
@@ -1,2 +1,12 @@
 # 백준 1966번
-# 문제 : 
+# 문제 : 8개 정수 있을 때 가장 큰 정수 5개 합이랑 몇 번째인지
+
+items = [int(input()) for _ in range(8)]
+idx = []
+
+for i in range(5) :
+    max_idx = items.index(max(items))
+    idx.append(max_idx)
+    
+    
+
